6c2e7be1c9c8cd25828d66324ddc861fd58da32d
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-02-27 12:12:45 -0800

    loading spinner added to app with delay

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-
-import HomePageCarousel from './HomePageCarousel';
+// import HomePageCarousel from './HomePageCarousel';
 import './HomePage.scss';
 import HomePageJumbotron from './Jumbotron/Jumbotron'
 import HomePageSiteDescription from './SiteDescription/SiteDescription'
+// import LoadingSpinner from '../LoadingSpinner';
 
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  componentDidMount() {
+
+
+  }
+
   render() {
+    // if (this.state.loading) {
+    //   return (
+    //     <div className="HomePage">
+    //       <LoadingSpinner />
+    //     </div>
+    //   )
+    // }
+
     return (
     <main>
       <div className="HomePage">
