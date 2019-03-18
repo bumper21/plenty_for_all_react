189ec412150bc4b2d7c8666a413d0db6e3a8e723
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-03-18 11:40:52 -0700

    sessions sign in and sign out requests added

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,25 @@
 const BASE_URL = `http://localhost:3030/api/`;
 
 
+export const Se0ssion = {
+  create(params) {
+    return fetch(`${BASE_URL}/sign_in` {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }).then(res => res.json());
+  },
+
+  destroy() {
+    return fetch(`${BASE_URL}/sign_out` {
+      method: "DELETE",
+      credentials: "include"
+    }).then(res => res.json())
+  }
+};
 
 export const User = {
   current() {
