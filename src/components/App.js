2347c2c1e6ead8a9f688b29f48ed0b1df11dd48f
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-03-21 22:12:19 -0700

    getUser and destroy session implemented but currentUser not functioning yet

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,44 @@ import HomePage from './HomePage/HomePage';
 import NavBar from './NavBar/NavBar';
 import LoadingSpinner from './LoadingSpinner';
 
+import { User, Session } from '../requests';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      loading: true
+      loading: true,
+      currentUser: null
     }
 
+    this.getUser = this.getUser.bind(this);
+    this.destroySession = this.destroySession.bind(this);
+
+  }
+
+  getUser() {
+    User.current().then(currentUser => {
+      if (currentUser.id) {
+        this.setState({ currentUser });
+      }
+    })
+  };
+
+  destroySession() {
+    Session.destroy().then(() => this.setState({
+      currentUser: null }));
   }
 
   componentDidMount() {
     setTimeout(function() {
       this.setState({loading: false})
     }.bind(this), 1000)
-   
-  }
+    this.getUser()
+  };
 
   render() {
+    const { currentUser } = this.state;
 
     if (this.state.loading) {
       return (
@@ -36,13 +56,19 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <NavBar />
+          <NavBar
+            currentUser={currentUser} onSignOut={this.destroySession} />
           <Switch>
             <Route
               path="/"
               exact
               component={HomePage}
             />
+            {/* <Route
+              path="/user/sign-in"
+              exact
+              component={}
+            /> */}
           </Switch>
         </div>
       </BrowserRouter>
